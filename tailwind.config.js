2cd1c10a2d557f330e4d886467d531bbd320f82c
guillaumegraglia-debug <guillaume.graglia@gmail.com>
2025-08-19 18:28:20 +0200

    feat: add design tokens and default font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,25 @@
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from "tailwindcss/defaultTheme";
+
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      colors: {
+        primary: "#1E3A8A",
+        secondary: "#64748B",
+      },
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+      },
+      spacing: {
+        128: "32rem",
+        144: "36rem",
+      },
+      borderRadius: {
+        "4xl": "2rem",
+      },
+    },
+  },
   plugins: [],
 };
