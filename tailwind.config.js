4acdaf534865b72cdcb923792e0af6aa3ec0900d
guillaumegraglia-debug <guillaume.graglia@gmail.com>
2025-08-19 18:23:38 +0200

    feat: extend theme with design tokens

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      colors: {
+        primary: "#1D4ED8",
+        secondary: "#64748B",
+      },
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
+      spacing: {
+        128: "32rem",
+        144: "36rem",
+      },
+      borderRadius: {
+        "4xl": "2rem",
+        "5xl": "2.5rem",
+      },
+    },
+  },
   plugins: [],
 };
