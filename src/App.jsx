1f1ccf693dbdc3e01743b9ff4868c93226f15b8f
guillaumegraglia-debug <guillaume.graglia@gmail.com>
2025-08-19 15:43:22 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-function App() {
+import BACSForm from "./BACSForm";
+
+export default function App() {
   return (
     <div className="app-container">
       <div className="card">
         <h1>Vérification Décret BACS</h1>
         <p>Complétez les informations pour savoir si vous êtes assujetti.</p>
 
-        {/* Ici tes formulaires Module 1 / Module 2 */}
+        {/* >>> ICI on rend le formulaire <<< */}
+        <BACSForm />
       </div>
     </div>
   );
 }
-
-export default App;
